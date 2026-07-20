5e9daa430a4bfdc648107b3b79c081b7d84c75b5
Peter Schwips <121677584+peterschwps@users.noreply.github.com>
2026-07-20 16:13:38 +0200

    refactor: renamed cli.py to app and moved it to cli folder

--- src/datasphere_cli/cli/app.py
+++ src/datasphere_cli/cli/app.py
@@ -18,15 +18,15 @@ def main(argv: Sequence[str] | None = None) -> int:
     # Non-interactive mode: if arguments are provided, run the direct command
     arguments = list(sys.argv[1:] if argv is None else argv)
     if arguments:
-        from datasphere_cli.direct import run
+        from datasphere_cli.cli.commands import run
 
         return run(arguments)
 
     # Non-interactive mode: if no arguments are provided, start the TUI
-    from datasphere_cli.utils.filehandler import file_setup
-    from datasphere_cli.utils.logging import configure_logging
-    from datasphere_cli.utils.screens import DatasphereApp
-    from datasphere_cli.utils.settings import load_settings
+    from datasphere_cli.cli.screens import DatasphereApp
+    from datasphere_cli.files.workspace import file_setup
+    from datasphere_cli.logging import configure_logging
+    from datasphere_cli.settings import load_settings
 
     configure_logging()
     load_settings()
