3092dd5ea4af93d0d80ba5b675ed2eb37a2b0d70
Peter Schwips <121677584+peterschwps@users.noreply.github.com>
2026-07-22 08:54:24 +0200

    chore: removed __all__ now only importing required objects

--- packages/datasphere-core/src/datasphere_core/__init__.py
+++ packages/datasphere-core/src/datasphere_core/__init__.py
@@ -1,5 +1,9 @@
+# ruff: noqa: F401
+
 from datasphere_core.auth import DatasphereSession, SessionConfig
-from datasphere_core.concurrency import execute_with_concurrency_limit
+from datasphere_core.concurrency import (
+    execute_with_concurrency_limit,
+)
 from datasphere_core.context import (
     CheckpointCallback,
     CommandContext,
@@ -19,39 +23,4 @@ from datasphere_core.errors import (
     TokenStoreError,
 )
 from datasphere_core.execution import execute_command
-from datasphere_core.models import (
-    MAXIMUM_BATCH_CONCURRENCY,
-    BatchItemResult,
-    BatchSummary,
-    CommandProgress,
-    CommandProgressPhase,
-    validate_max_concurrency,
-)
 from datasphere_core.registry import COMMANDS
-
-__all__ = [
-    "COMMANDS",
-    "MAXIMUM_BATCH_CONCURRENCY",
-    "BatchSummary",
-    "BatchItemResult",
-    "CommandCancelledError",
-    "CheckpointCallback",
-    "CommandContext",
-    "CommandDefinition",
-    "CommandError",
-    "CommandHandler",
-    "CommandProgress",
-    "CommandProgressPhase",
-    "CommandRegistry",
-    "CommandTimeoutError",
-    "DatasphereSession",
-    "KeyringTokenStore",
-    "ProgressCallback",
-    "SessionConfig",
-    "SessionNotAuthenticatedError",
-    "TokenStore",
-    "TokenStoreError",
-    "execute_command",
-    "execute_with_concurrency_limit",
-    "validate_max_concurrency",
-]
